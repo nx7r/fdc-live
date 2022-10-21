abc9b332758849e468a60ebb94b07246ad9cdf4e
Djawad <78606454+nx7r@users.noreply.github.com>
2022-10-22 00:51:28 +0100

    Add files via upload

--- fdc/settings.py
+++ fdc/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xv-0!!613q2h-g-i#i3bb@@_+%pqggp!p71k)t@^8q6(!jtn57'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['fdc-club.herokuapp.com','127.0.0.1']
 
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
